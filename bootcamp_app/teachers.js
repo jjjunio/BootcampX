b888b40c14bdf94c78cccd317a8adc3e09c5a62c
Jason Junio <jjjunio@gmail.com>
2020-05-28 00:23:13 +0000

    created student.js and teachers.js to test connection and practice query to database

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,14 +11,16 @@ const cohortName = process.argv[2];
 const maxResults = process.argv[3];
 
 pool.query(`
-SELECT students.id as id, students.name as name, cohorts.name as cohort
-FROM students
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohortName}%'
-LIMIT ${maxResults};
+WHERE cohorts.name = '${cohortName}'
+ORDER BY teacher;
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohort}: ${user.teacher}`);
   })
 }).catch(err => console.error('query error', err.stack));
